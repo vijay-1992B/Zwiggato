9986652f6b9f7e4ac68688788408b23dcf9bf9fe
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-09-15 14:14:57 +0530

    Update useRestaurantMenu.js

--- src/utils/hooks/useRestaurantMenu.js
+++ src/utils/hooks/useRestaurantMenu.js
@@ -10,7 +10,7 @@ const useRestaurantMenu = (resId) => {
 
 
   const fetchData = async () => {
-    const data = await fetch(RESTAURANT_MENU_API + resId);
+    const data = await fetch("/api/proxy?url="+RESTAURANT_MENU_API + resId);
     const json = await data.json();
     setResInfo(json);
   };
