8c1c2db022f3f5192e0a071c52cec76c35f36c9e
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-09-18 17:41:24 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RESTAURANT_LIST_API =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?";
 
 export const RESTAURANT_MENU_API =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6186784&lng=77.05335339999999&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6186784&lng=77.05335339999999&restaurantId=";
 
 export const NO_IMAGE_URL =
   "https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg";
