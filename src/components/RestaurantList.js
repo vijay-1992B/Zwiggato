136364b4724ae85a2008d63cc02d059843cb92df
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-09-18 17:38:06 +0530

    /

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -49,7 +49,7 @@ const RestaurantList = () => {
   const fetchData = async () => {
     const raw = await fetch(RESTAURANT_LIST_API + `lat=${lat}&lng=${lng}`, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
       },
     });
 
