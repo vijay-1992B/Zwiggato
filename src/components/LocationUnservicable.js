c37dd9216721403f7b89651fc65dca6a9c6b8deb
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-10-05 10:31:49 +0530

    Update LocationUnservicable.js

--- src/components/LocationUnservicable.js
+++ src/components/LocationUnservicable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LocationUnservicable = () => {
   return (
@@ -7,6 +8,7 @@ const LocationUnservicable = () => {
       <img className="size-60" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_476,h_476/portal/m/location_unserviceable.png" />
       <h1 className="  sm:text-lg mt-4 mb-1 font-extrabold">Location Unserviceable</h1>
       <p className="w-[360px] text-sm sm:text-md  text-center">We don’t have any services here till now. Try changing location.</p>
+      <Link to={"/"}><button className="self-center w-[150px] my-5 px-4 py-2 rounded-md text-white bg-[#CB202D] text-center">Back to home</button></Link>
       </div>
     </div>
   );
